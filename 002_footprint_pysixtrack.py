15458c9b1ac3bff8f0f2c3548b7cc2344b42cf9b
giadarol <giovanni.iadarola@cern.ch>
2018-11-17 01:20:37 +0100

    Footprint very distorted

--- 002_footprint_pysixtrack.py
+++ 002_footprint_pysixtrack.py
@@ -1,14 +1,16 @@
 import pickle
 import pysixtrack
 import numpy as np
+import NAFFlib
 
 n_turns = 100
 epsn_x = 3.5e-6
 epsn_y = 3.5e-6
-N_sigma_footp = 7.
-N_theta_footp = 5.
+r_max_sigma = 2.
+N_r_footp = 3.
+N_theta_footp = 3.
 
-def track_particle(line, part):
+def track_particle(line, part, verbose=False):
 
     x_tbt = np.zeros(n_turns)
     px_tbt = np.zeros(n_turns)
@@ -18,7 +20,9 @@ def track_particle(line, part):
     delta_tbt = np.zeros(n_turns)
 
     for i_turn in range(n_turns):
-        print('Turn %d/%d'%(i_turn, n_turns))
+        if verbose:
+            print('Turn %d/%d'%(i_turn, n_turns))
+
         for name, etype, ele in line:
             ele.track(part)
 
@@ -58,10 +62,10 @@ with open('particle_on_CO.pkl', 'rb') as fid:
 part = pysixtrack.Particles(**partCO)
 
 # Track a particle to get betas
-part.x = 1e-5
-part.y = 1e-5
+part.x += 1e-5
+part.y += 1e-5
 
-x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = track_particle(line, part)
+x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = track_particle(line, part, verbose=True)
 
 beta_x, x_max, px_cut = betafun_from_ellip(x_tbt, px_tbt)
 beta_y, y_max, py_cut = betafun_from_ellip(y_tbt, py_tbt)
@@ -70,11 +74,25 @@ sigmax = np.sqrt(beta_x * epsn_x / part.beta0 / part.gamma0)
 sigmay = np.sqrt(beta_y * epsn_y / part.beta0 / part.gamma0)
 
 import footprint
-xy_norm = footprint.initial_xy_polar(r_min = 1e-2, r_max=N_sigma_footp, r_N=N_sigma_footp+1,
-    theta_min=0., theta_max=np.pi/2, theta_N = N_theta_footp)
+xy_norm = footprint.initial_xy_polar(r_min=1e-2, r_max=r_max_sigma, r_N=N_r_footp + 1,
+                                     theta_min=0., theta_max=np.pi / 2, theta_N=N_theta_footp)
 
 # Tracking for footprint
-#...
+Qxy_fp = np.zeros_like(xy_norm)
+for ii in range(xy_norm.shape[0]):
+    for jj in range(xy_norm.shape[1]):
+        print('FP track %d/%d (%d/%d)'%(ii, xy_norm.shape[0], jj, xy_norm.shape[1]))
+        part = pysixtrack.Particles(**partCO)
+        part.x += xy_norm[ii, jj, 0] * sigmax
+        part.y += xy_norm[ii, jj, 1] * sigmay
+
+        x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = track_particle(line, part)
+
+        qx = NAFFlib.get_tune(x_tbt)
+        qy = NAFFlib.get_tune(y_tbt)
+
+        Qxy_fp[ii, jj, 0] = qx
+        Qxy_fp[ii, jj, 1] = qy
 
 import matplotlib.pyplot as plt
 plt.close('all')
@@ -100,5 +118,11 @@ axcoord = fig3.add_subplot(1, 1, 1)
 footprint.draw_footprint(xy_norm, axis_object=axcoord)
 axcoord.set_xlim(right=np.max(xy_norm[:, :, 0]))
 axcoord.set_ylim(top=np.max(xy_norm[:, :, 1]))
+
+fig4 = plt.figure(4)
+axFP = fig4.add_subplot(1, 1, 1)
+footprint.draw_footprint(Qxy_fp, axis_object=axFP)
+axFP.set_xlim(right=np.max(Qxy_fp[:, :, 0]))
+axFP.set_ylim(top=np.max(Qxy_fp[:, :, 1]))
 
 plt.show()
